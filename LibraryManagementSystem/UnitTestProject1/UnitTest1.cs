32c243b505e0c0880eeb38d4abc73d4fb005cf13
Sakshi Sharma <sakshis@tavisca.com>
2017-08-23 14:35:04 +0530

    all modules added

---
+++ LibraryManagementSystem/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using LibraryManagementSystem;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void BookFunctionalityCheck()
+        {
+            List<string> authorname = new List<string>();
+            authorname.Add("Eric Freeman");
+            authorname.Add("Eric Lippert");
+            Books books = new Books("Head First Design Pattern",Genre.Selfhelp, 13452763, authorname, "ABC publications", 5);
+            Assert.AreEqual("Hallary Halamn",books.AuthorName);
+           
+
+        }
+    }
+}
